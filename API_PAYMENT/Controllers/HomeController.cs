41c3f44bd9b2650200ff3016456be73cd440885e
Fikri Ardiansyah <fikri.ardiansyah@corp.bri.co.id>
2019-01-28 14:01:29 +0700

    APIPAYMENT-1 COBA 3

--- API_PAYMENT/Controllers/HomeController.cs
+++ API_PAYMENT/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace API_PAYMENT.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Pages Wkwk2";
+            ViewBag.Title = "Home Pages Wkwk2 3";
 
             return View();
         }
