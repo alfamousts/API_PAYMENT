0e5e063d3fec45e37d99ca262b44daed3207d92b
Hanum <latifah.hanum@corp.bri.co.id>
2019-01-30 19:52:07 +0700

    APIPAYMENT-14 tambah cek ada di tabel feature map apa enggak

--- API_PAYMENT/Models/TelkomValidation.cs
+++ API_PAYMENT/Models/TelkomValidation.cs
@@ -8,27 +8,35 @@ namespace API_PAYMENT.Models
     public class TelkomValidation
     {
         TelkomHelper telkomHelper = new TelkomHelper();
+        Helper helper = new Helper();
 
         public string ValidateInputInquiryTelkom(ref TelkomModels.TelkomInquiryRequest AutoInqRequest)
         {
             decimal number;
             string rc = "";
 
-            if (String.IsNullOrEmpty(AutoInqRequest.BillingNumber))
+            if (helper.CheckFeature(AutoInqRequest.InstitutionCode, ConstantModels.FeatureCode_Telkom))
             {
-                rc = "0210";
-            }
-            else if (String.IsNullOrEmpty(AutoInqRequest.SourceAccount))
-            {
-                rc = "0211";
-            }
-            else if (!decimal.TryParse(AutoInqRequest.BillingNumber, out number))
-            {
-                rc = "0204";
+                if (String.IsNullOrEmpty(AutoInqRequest.BillingNumber))
+                {
+                    rc = "0210";
+                }
+                else if (String.IsNullOrEmpty(AutoInqRequest.SourceAccount))
+                {
+                    rc = "0211";
+                }
+                else if (!decimal.TryParse(AutoInqRequest.BillingNumber, out number))
+                {
+                    rc = "0204";
+                }
+                else
+                {
+                    rc = "0005"; //Validasi sukses
+                }
             }
             else
             {
-                rc = "0005"; //Validasi sukses
+                rc = "0011";
             }
 
             return rc;
@@ -40,52 +48,59 @@ namespace API_PAYMENT.Models
             decimal number;
             string rc = "";
 
-            if (!ceknoref)
+            if (helper.CheckFeature(PayRequest.InstitutionCode, ConstantModels.FeatureCode_Telkom))
             {
-                if (String.IsNullOrEmpty(PayRequest.TotalAmount))
+                if (!ceknoref)
                 {
-                    rc = "0205"; //Total amount tidak boleh kosong
-                }
-                else if (String.IsNullOrEmpty(PayRequest.BillingNumber))
-                {
-                    rc = "0210"; //Billing number tidak boleh kosong
-                }
-                else if (String.IsNullOrEmpty(PayRequest.SourceAccount))
-                {
-                    rc = "0211"; //Source account tidak boleh kosong
-                }
-                else if (String.IsNullOrEmpty(PayRequest.Name))
-                {
-                    rc = "0206"; //Name tidak boleh kosong
-                }
-                else if (String.IsNullOrEmpty(PayRequest.BillingCode))
-                {
-                    rc = "0207"; //Billing code tidak boleh kosong
-                }
-                else if (String.IsNullOrEmpty(PayRequest.ReferralNumber))
-                {
-                    rc = "0014"; //Referral number tidak boleh kosong
-                }
-                else if (!decimal.TryParse(PayRequest.TotalAmount.Replace(",", ""), out number))
-                {
-                    rc = "0208"; //Total amount mengandung karakter bukan angka
-                }
-                else if (System.Convert.ToDouble(PayRequest.TotalAmount.Replace(",", "")) < 1)
-                {
-                    rc = "0209"; //Total amount tidak boleh 0 atau bernilai negatif
-                }
-                else if (!decimal.TryParse(PayRequest.ReferralNumber, out number))
-                {
-                    rc = "0012"; //Referral number mengandung karakter bukan angka
+                    if (String.IsNullOrEmpty(PayRequest.TotalAmount))
+                    {
+                        rc = "0205"; //Total amount tidak boleh kosong
+                    }
+                    else if (String.IsNullOrEmpty(PayRequest.BillingNumber))
+                    {
+                        rc = "0210"; //Billing number tidak boleh kosong
+                    }
+                    else if (String.IsNullOrEmpty(PayRequest.SourceAccount))
+                    {
+                        rc = "0211"; //Source account tidak boleh kosong
+                    }
+                    else if (String.IsNullOrEmpty(PayRequest.Name))
+                    {
+                        rc = "0206"; //Name tidak boleh kosong
+                    }
+                    else if (String.IsNullOrEmpty(PayRequest.BillingCode))
+                    {
+                        rc = "0207"; //Billing code tidak boleh kosong
+                    }
+                    else if (String.IsNullOrEmpty(PayRequest.ReferralNumber))
+                    {
+                        rc = "0014"; //Referral number tidak boleh kosong
+                    }
+                    else if (!decimal.TryParse(PayRequest.TotalAmount.Replace(",", ""), out number))
+                    {
+                        rc = "0208"; //Total amount mengandung karakter bukan angka
+                    }
+                    else if (System.Convert.ToDouble(PayRequest.TotalAmount.Replace(",", "")) < 1)
+                    {
+                        rc = "0209"; //Total amount tidak boleh 0 atau bernilai negatif
+                    }
+                    else if (!decimal.TryParse(PayRequest.ReferralNumber, out number))
+                    {
+                        rc = "0012"; //Referral number mengandung karakter bukan angka
+                    }
+                    else
+                    {
+                        rc = "0005"; //Success
+                    }
                 }
                 else
                 {
-                    rc = "0005"; //Success
+                    rc = "0013"; //Nomor referral sudah pernah digunakan
                 }
             }
             else
             {
-                rc = "0013"; //Nomor referral sudah pernah digunakan
+                rc = "0011";
             }
 
             return rc;
